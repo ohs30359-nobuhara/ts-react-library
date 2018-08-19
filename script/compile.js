ffab877ac8ef6210a2ee94632684a3dee626bd23
信原　有志 <ynobuhar@MBP15JAL-NC0061.local>
2018-08-19 22:20:36 +0900

    buildタスクを修正

--- script/compile.js
+++ script/compile.js
@@ -3,42 +3,50 @@ const path = require('path');
 
 /**
  * 各component module を全て root配下に展開し
- * 利用側で import Module from 'react-atoms/lib/dir/module' 形式で呼べるようにする
+ * 利用側で import Module from 'ts-react-library/lib/dir/module' 形式で呼べるようにする
  * @param rootPath
  * @param dir
  * @returns {{}}
  */
-const loader = (rootPath, dir) => {
+const loader = (rootPath) => {
   const basePath = path.resolve(rootPath, 'src/');
-  const targets = glob.sync(`${basePath}/${dir}/*`);
+  const targets = glob.sync(`${basePath}/**/*`);
 
   const entries = {};
 
   targets.forEach(value => {
-    const re = new RegExp(`${basePath}/${dir}/`);
-    const key = `${dir}/${value.replace(re, '')}`;
+    const re = new RegExp(`${basePath}/`);
+    let key = `${value.replace(re, '')}`;
+
+    // TODO: サポートされていない正規表現 サポートされたら変える
+    //if (! key.match( /(?!.stories)\.(tsx|ts)$/)) return;
+
+    // storybook以外のts tsx
+    if ( key.match(/.stories./)) return;
+    if (! key.match(/\.(ts|tsx)$/)) return;
+
+    // test ディレクトリは対象外
+    if (key.match(/__test__/)) return;
+
+    // 拡張子を削除 して 全て js変換
+    key = key.replace(/\.(ts|tsx)$/, '') + '.js';
+
     entries[key] = value;
   });
+
+  console.log(entries)
 
   return entries;
 };
 
 
 module.exports = (rootPath) => {
-
-  // dist対象ディレクトリ
-  const distConfig = [
-    'components',
-  ];
-
   // package entry point
   let entry = {
-    'index': path.resolve(rootPath, 'src/index.ts')
+    'index.js': path.resolve(rootPath, 'src/index.ts')
   };
 
-  distConfig.forEach(config => {
-    entry = Object.assign(entry, loader(rootPath, config))
-  });
+  entry = Object.assign(entry, loader(rootPath))
 
   return entry
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,33 @@ const nodeExternals = require('webpack-node-externals');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const loader = require('./script/compile');
 
-module.exports = {
+const cssConfig =  {
+  entry: {
+    style: './src/style/index.scss'
+  },
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: '[name].css'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+    })
+  ]
+}
+
+const jsConfig = {
   entry: loader(__dirname),
   output: {
     path: path.resolve(__dirname, './lib'),
@@ -18,20 +44,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx|ts$/,
+        test: /(?!.stories)\.(tsx|ts)$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
       }
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-  ]
-};
+  }
+}
+
+
+module.exports = [jsConfig, cssConfig];
